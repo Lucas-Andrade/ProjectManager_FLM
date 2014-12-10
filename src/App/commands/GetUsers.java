f6a4030c4b39e671d4eb49f024be1079ff79319f
filipe-maia <filipemaia@msn.com>
2014-12-10 10:48:28 +0000

    alterei o databaseelements e o getusers

--- src/App/commands/GetUsers.java
+++ src/App/commands/GetUsers.java
@@ -1,40 +1,55 @@
 package App.commands;
 
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Map;
 
+import App.elements.User;
+import App.elements.UserInterface;
 import App.repository.UsersRepository;
 
 /**
  * @author amiguinhos do Maia
  * GET /users - retorna a lista de utilizadores
  */
-public class GetUsers implements Command {
+public class GetUsers implements Command
+{
+
+	private final UsersRepository repository;
 
 	/**
-	 * Class that implements the {@link GetUser} factory, according to the 
-	 * AbstratFactory design pattern. 
+	 * Class that implements the {@link GetUser} factory, according to the
+	 * AbstratFactory design pattern.
 	 */
-	public static class Factory implements CommandFactory 
+	public static class Factory implements CommandFactory
 	{
 		private final UsersRepository repository;
-		
+
 		public Factory(UsersRepository repository)
 		{
 			this.repository = repository;
 		}
-		
+
 		@Override
-		public Command newInstance(Map<String, String> parameters) 
+		public Command newInstance(Map<String, String> parameters)
 		{
-			// TODO
-			return null;
+			return new GetUsers(repository);
 		}
-	
-}
-@Override
-public void execute()
-{
-	// TODO
-}
+	}
+
+	private GetUsers(UsersRepository repository)
+	{
+		this.repository = repository;
+	}
+
+	@Override
+	public void execute(OutputStream writer) throws IOException
+	{
+		for(UserInterface user : repository.getDatabaseElements())
+		{
+			writer.write(user.toString().getBytes());
+			writer.close();
+		}
+	}
 
 }
