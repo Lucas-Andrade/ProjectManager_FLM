c11208dcc3eaae5fe0343971d11ec3b9340c624f
Lucas <lucas.andrade234@gmail.com>
2014-12-10 11:07:43 +0000

    no message

--- src/App/commands/Command.java
+++ src/App/commands/Command.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package App.commands;
 
 import java.io.IOException;
@@ -13,20 +13,8 @@ import java.io.OutputStream;
 public interface Command 
 {
 	void execute(OutputStream out) throws IOException;
-}
-=======
-package App.commands;
-
-
-// ainda vai ser alterada
-
-/**
- * Contract to be supported by all commands. Instances cannot be executed multiple times.
- */
-public interface Command 
-{
-	void execute();
 	// TODO em vez dum OutputStream, não seria melhor um Writer? Nós só queremos
-	// uma stream de caracteres, ou no futuro queremos mais?
+		// uma stream de caracteres, ou no futuro queremos mais?
 }
->>>>>>> origin/paraFazerMerge
+
+
