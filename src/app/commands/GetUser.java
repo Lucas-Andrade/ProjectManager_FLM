722a39ff4af9c28d2f3468427f80604087630c9f
filipe-maia <filipemaia@msn.com>
2014-12-14 14:27:16 +0000

    comANdos feitos

--- src/app/commands/GetUser.java
+++ src/app/commands/GetUser.java
@@ -9,62 +9,96 @@ import app.repository.UserRepository;
 import app.resultsOutputMethods.ResultOutputMethod;
 
 /**
- * @author amiguinhos do Maia
- *GET /users/{username} - retorna informação sobre o utilizador com nome de utilizador username.
+ * Class whose instances are {@link Command}s that return a {@link User}.
  */
 public class GetUser extends BaseCommand
 {
-	
-	private String username;
-	private final UserRepository repository;
-	
+
 	/**
-	 * The {@link CommandParser.Node.content} to be used (between "{" and "}", see
-	 * {@link CommandParser#Node#isPlaceHolderNode()}) in the
-	 * {@link CommandParser#Node} that has the {@code GetUser#Factory} in
-	 * the field {@link CommandParser#Node#factory}.
+	 * {@code String} with the {@code User}'s Username argument. This argument
+	 * is used for getting the right {@code User} from the
+	 * {@code UserRepository}.
+	 */
+	private String username;
+
+	/**
+	 * The {@link UserRepository} with the {@code User}s. This
+	 * {@code UserRepository} is accessed to get the {@code User}.
+	 */
+	private final UserRepository repository;
+
+	/**
+	 * The {@link CommandParser.Node.content} to be used (between "{" and "}",
+	 * see {@link CommandParser#Node#isPlaceHolderNode()}) in the
+	 * {@link CommandParser#Node} that has the {@code GetUser#Factory} in the
+	 * field {@link CommandParser#Node#factory}.
 	 */
 	public static final String USERNAME = "username";
-	
+
 	/**
-	 * Class that implements the {@link GetUser} factory, according to the 
-	 * AbstratFactory design pattern. 
+	 * Class that implements the {@link GetUser} factory, according to the
+	 * AbstratFactory design pattern.
 	 */
-	public static class Factory implements CommandFactory 
+	public static class Factory implements CommandFactory
 	{
+
+		/**
+		 * The {@link UserRepository} with the {@code User}s. This
+		 * {@code UserRepository} is accessed to get the {@code User}.
+		 */
 		private final UserRepository repository;
-		
+
+		/**
+		 * The constructor for {@code Factory}.
+		 * 
+		 * @param repository
+		 *            The {@code UserRepository}.
+		 */
 		public Factory(UserRepository repository)
 		{
 			this.repository = repository;
 		}
-		
+
 		/**
-		 * @see CommandFactory.newInstance
-		 * Suplies the {@code GetUser} constructor with a {@code UsersRepository}
-		 * and with the parameter {@link pathholderParameter} stored inside the
-		 * Map {@link parameters}, to generate a new instance of {@code GetUser}.
+		 * @see CommandFactory#newInstance(Map)
 		 */
 		@Override
-		public Command newInstance(Map<String, String> parameters) 
+		public Command newInstance(Map<String, String> parameters)
 		{
 			return new GetUser(repository, parameters);
 		}
 	}
-	
 
+	/**
+	 * The constructor for {@code GetUser}.
+	 * 
+	 * @param repository
+	 *            The {@code UserRepository}.
+	 * @param parameters
+	 *            The {@code Command} arguments.
+	 */
 	private GetUser(UserRepository repository, Map<String, String> parameters)
 	{
 		super(parameters);
-		this.username=parameters.get(USERNAME);
-		this.repository=repository;
+		this.username = parameters.get(USERNAME);
+		this.repository = repository;
 	}
 
+	/**
+	 * @see app.commands.BaseCommand#getMandatoryParameters()
+	 */
 	@Override
-	protected String[] getMandatoryParameters() {
-		return new String[] {USERNAME};
+	protected String[] getMandatoryParameters()
+	{
+		return new String[] { USERNAME };
 	}
 
+	/**
+	 * Get's the {@code User} with the {@code GetUser#username} in the
+	 * {@code UserRepository}, if it exists.
+	 * 
+	 * @see BaseCommand#internalExecute(ResultOutputMethod)
+	 */
 	@Override
 	protected void internalExecute(ResultOutputMethod out)
 			throws CommandException, IOException
--- src/app/commands/GetUsers.java
+++ src/app/commands/GetUsers.java
@@ -8,48 +8,84 @@ import app.repository.UserRepository;
 import app.resultsOutputMethods.ResultOutputMethod;
 
 /**
- * @author amiguinhos do Maia
- * GET /users - retorna a lista de utilizadores
+ * Class whose instances are {@link Command}s that return the {@link User}s.
  */
 public class GetUsers extends BaseCommand
 {
 
-	private final UserRepository repository;
-	
 	/**
-	 * Class that implements the {@link GetUser} factory, according to the
-	 * AbstratFactory design pattern.
+	 * The {@link UserRepository} with the {@code User}s. This
+	 * {@code UserRepository} is accessed to get the {@code User}s.
+	 */
+	private final UserRepository repository;
+
+	/**
+	 * Class that implements the {@link GetUsers} factory, according to the
+	 * {@link CommandFactory}.
 	 */
 	public static class Factory implements CommandFactory
 	{
+
+		/**
+		 * The {@link UserRepository} with the {@code User}s. This
+		 * {@code UserRepository} is accessed to get the {@code User}s.
+		 */
 		private final UserRepository repository;
 
+		/**
+		 * The constructor for {@code Factory}.
+		 * 
+		 * @param uRepository
+		 *            The {@code UserRepository}.
+		 */
 		public Factory(UserRepository repository)
 		{
 			this.repository = repository;
 		}
 
+		/**
+		 * @see CommandFactory#newInstance(Map)
+		 */
 		@Override
 		public Command newInstance(Map<String, String> parameters)
 		{
 			return new GetUsers(repository, parameters);
 		}
+
 	}
-	
+
+	/**
+	 * The constructor for {@code GetUsers}.
+	 * 
+	 * @param repository
+	 *            The {@code UserRepository}.
+	 * @param parameters
+	 *            The {@code Command} arguments.
+	 */
 	public GetUsers(UserRepository repository, Map<String, String> parameters)
 	{
 		super(parameters);
 		this.repository = repository;
 	}
-	
+
+	/**
+	 * Get's all the {@code User}s in the {@code UserRepository}.
+	 * 
+	 * @see BaseCommand#internalExecute(ResultOutputMethod)
+	 */
 	@Override
-	public void internalExecute(ResultOutputMethod out) throws CommandException, IOException
+	public void internalExecute(ResultOutputMethod out)
+			throws CommandException, IOException
 	{
-		out.giveResults(repository.getAllUsers());
+		out.giveResults((Object[]) repository.getAllUsers());
 	}
 
+	/**
+	 * @see app.commands.BaseCommand#getMandatoryParameters()
+	 */
 	@Override
-	protected String[] getMandatoryParameters() {
+	protected String[] getMandatoryParameters()
+	{
 		return null;
 	}
 
