95c2d3eaeaf1243cca046b76ccd402fa470b3cd2
FilipaG <filipa.isabel.goncalves@gmail.com>
2015-01-12 09:54:01 +0000

    actualizações fds

--- src/main/java/swing/PatchProject.java
+++ src/main/java/swing/PatchProject.java
@@ -2,15 +2,35 @@ package swing;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
 public class PatchProject extends JDialog {
 
-	private final JPanel contentPanel = new JPanel();
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private final JPanel patchProjectPanel = new JPanel();
+	private JTextField LatitudeField;
+	private JTextField longitudeField;
+	private JTextField nameField;
+	private JTextField priceField;
 
 	/**
 	 * Launch the application.
@@ -18,6 +38,8 @@ public class PatchProject extends JDialog {
 	public static void main(String[] args) {
 		try {
 			PatchProject dialog = new PatchProject();
+			//definimos o título da janel
+			dialog.setTitle("Patch Project");
 			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 			dialog.setVisible(true);
 		} catch (Exception e) {
@@ -29,11 +51,209 @@ public class PatchProject extends JDialog {
 	 * Create the dialog.
 	 */
 	public PatchProject() {
-		setBounds(100, 100, 450, 300);
+		//Definição da Caixa de Diálogo
+		setBounds(100, 100, 636, 387);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
-		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
-		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		patchProjectPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(patchProjectPanel, BorderLayout.CENTER);
+		
+		// Definição do Painel interno
+		GridBagLayout gbl_patchProjectPanel = new GridBagLayout();  //Layout Manager
+		gbl_patchProjectPanel.columnWidths = new int[]{20, 0, 100, 100, 0, 60, 100, 0, 0};
+		gbl_patchProjectPanel.rowHeights = new int[]{0, 0, 30, 0, 30, 0, 30, 0, 0, 0, 0};
+		gbl_patchProjectPanel.columnWeights = new double[]{0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gbl_patchProjectPanel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		patchProjectPanel.setLayout(gbl_patchProjectPanel);
+		
+		
+		     //Inserir imagem : user -> Path e localização no Painel
+		{ 
+			BufferedImage myPicture;
+			try {
+				myPicture = ImageIO.read(new File("src\\main\\java\\swing\\imagens\\Project.jpg"));
+					
+				// Título do Comando : Post User
+				{
+					JLabel lblPatchProject = new JLabel("Patch Project");
+					lblPatchProject.setFont(new Font("OCR A Extended", Font.BOLD | Font.ITALIC, 14));
+					GridBagConstraints gbc_lblPatchProject = new GridBagConstraints();
+					gbc_lblPatchProject.gridwidth = 5;
+					gbc_lblPatchProject.insets = new Insets(0, 0, 5, 5);
+					gbc_lblPatchProject.gridx = 2;
+					gbc_lblPatchProject.gridy = 0;
+					patchProjectPanel.add(lblPatchProject, gbc_lblPatchProject);
+				}
+				
+				JLabel userLabel = new JLabel(new ImageIcon(myPicture));
+				GridBagConstraints gbc_lblUser = new GridBagConstraints();
+				gbc_lblUser.gridheight = 3;
+				gbc_lblUser.insets = new Insets(0, 0, 5, 5);
+				gbc_lblUser.anchor = GridBagConstraints.SOUTHEAST;
+				gbc_lblUser.gridx = 1;
+				gbc_lblUser.gridy = 4;
+				patchProjectPanel.add(userLabel, gbc_lblUser);
+			
+			} catch (IOException e) 
+			{
+				e.printStackTrace();
+			}
+
+		}
+		
+		// Informação sobre o user que está a usar o programa
+		{
+			JLabel lblWellcome = new JLabel("Bem-vindo");
+			GridBagConstraints gbc_lblWellcome = new GridBagConstraints();
+			gbc_lblWellcome.anchor = GridBagConstraints.BELOW_BASELINE_TRAILING;
+			gbc_lblWellcome.insets = new Insets(0, 0, 5, 5);
+			gbc_lblWellcome.gridx = 6;
+			gbc_lblWellcome.gridy = 1;
+			patchProjectPanel.add(lblWellcome, gbc_lblWellcome);
+		}
+		{
+			JLabel lblRegistryuser = new JLabel("RegistryUser");
+			GridBagConstraints gbc_lblRegistryuser = new GridBagConstraints();
+			gbc_lblRegistryuser.anchor = GridBagConstraints.WEST;
+			gbc_lblRegistryuser.insets = new Insets(0, 0, 5, 0);
+			gbc_lblRegistryuser.gridx = 7;
+			gbc_lblRegistryuser.gridy = 1;
+			patchProjectPanel.add(lblRegistryuser, gbc_lblRegistryuser);
+		}
+		
+		   // Labels e campos a ser preenchidos
+		{
+			JLabel lblProjectID = new JLabel("Project ID:");
+			GridBagConstraints gbc_lblProjectID = new GridBagConstraints();
+			gbc_lblProjectID.anchor = GridBagConstraints.SOUTHEAST;
+			gbc_lblProjectID.insets = new Insets(0, 0, 5, 5);
+			gbc_lblProjectID.gridx = 2;
+			gbc_lblProjectID.gridy = 3;
+			patchProjectPanel.add(lblProjectID, gbc_lblProjectID);
+		}
+		
+		//terá a lista dos projectos no repositório 
+		{
+			//elementos da lista
+			String[] petStrings = { "Bird", "Cat", "Dog", "Rabbit", "Pig" };
+			
+			//Create the combo box, select item at index 4.
+			//Indices start at 0, so 4 specifies the pig.
+			JComboBox projectComboBox = new JComboBox(petStrings);
+			GridBagConstraints gbc_projectComboBox = new GridBagConstraints();
+			gbc_projectComboBox.insets = new Insets(0, 0, 5, 5);
+			gbc_projectComboBox.fill = GridBagConstraints.HORIZONTAL;
+			gbc_projectComboBox.gridx = 3;
+			gbc_projectComboBox.gridy = 3;
+			patchProjectPanel.add(projectComboBox, gbc_projectComboBox);
+			projectComboBox.setEditable(true);
+			//projectComboBox.addActionListener(this);
+		}
+		
+			{
+				JLabel lblLocation = new JLabel("Location:");
+				GridBagConstraints gbc_lblLocation = new GridBagConstraints();
+				gbc_lblLocation.insets = new Insets(0, 0, 5, 5);
+				gbc_lblLocation.gridx = 2;
+				gbc_lblLocation.gridy = 5;
+				patchProjectPanel.add(lblLocation, gbc_lblLocation);
+			}
+		
+		
+		   
+		{
+			JLabel lblName = new JLabel("Name:");
+			GridBagConstraints gbc_lblName = new GridBagConstraints();
+			gbc_lblName.anchor = GridBagConstraints.EAST;
+			gbc_lblName.insets = new Insets(0, 0, 5, 5);
+			gbc_lblName.gridx = 2;
+			gbc_lblName.gridy = 6;
+			patchProjectPanel.add(lblName, gbc_lblName);
+		}
+		{
+			nameField = new JTextField();
+			nameField.setColumns(10);
+			GridBagConstraints gbc_nameField = new GridBagConstraints();
+			gbc_nameField.gridwidth = 2;
+			gbc_nameField.insets = new Insets(0, 0, 5, 5);
+			gbc_nameField.fill = GridBagConstraints.HORIZONTAL;
+			gbc_nameField.gridx = 3;
+			gbc_nameField.gridy = 6;
+			patchProjectPanel.add(nameField, gbc_nameField);
+		}
+		
+		{
+			JLabel lblPrice = new JLabel("Price:");
+			GridBagConstraints gbc_lblPrice = new GridBagConstraints();
+			gbc_lblPrice.anchor = GridBagConstraints.EAST;
+			gbc_lblPrice.insets = new Insets(0, 0, 5, 5);
+			gbc_lblPrice.gridx = 5;
+			gbc_lblPrice.gridy = 6;
+			patchProjectPanel.add(lblPrice, gbc_lblPrice);
+			lblPrice.setToolTipText("Euros");
+
+		}
+		{
+			priceField = new JTextField();
+			priceField.setColumns(10);
+			GridBagConstraints gbc_priceField = new GridBagConstraints();
+			gbc_priceField.insets = new Insets(0, 0, 5, 5);
+			gbc_priceField.fill = GridBagConstraints.HORIZONTAL;
+			gbc_priceField.gridx = 6;
+			gbc_priceField.gridy = 6;
+			patchProjectPanel.add(priceField, gbc_priceField);
+		}
+		{
+			JLabel label = new JLabel("Euros");
+			GridBagConstraints gbc_label = new GridBagConstraints();
+			gbc_label.anchor = GridBagConstraints.WEST;
+			gbc_label.insets = new Insets(0, 0, 5, 0);
+			gbc_label.gridx = 7;
+			gbc_label.gridy = 6;
+			patchProjectPanel.add(label, gbc_label);
+		}
+		
+		{
+			JLabel lblLatitude = new JLabel("Latitude:");
+			GridBagConstraints gbc_lblLatitude = new GridBagConstraints();
+			gbc_lblLatitude.anchor = GridBagConstraints.EAST;
+			gbc_lblLatitude.insets = new Insets(0, 0, 5, 5);
+			gbc_lblLatitude.gridx = 2;
+			gbc_lblLatitude.gridy = 7;
+			patchProjectPanel.add(lblLatitude, gbc_lblLatitude);
+		}
+		
+		{
+			LatitudeField = new JTextField();
+			GridBagConstraints gbc_latitudeField = new GridBagConstraints();
+			gbc_latitudeField.fill = GridBagConstraints.HORIZONTAL;
+			gbc_latitudeField.insets = new Insets(0, 0, 5, 5);
+			gbc_latitudeField.gridx = 3;
+			gbc_latitudeField.gridy = 7;
+			patchProjectPanel.add(LatitudeField, gbc_latitudeField);
+			LatitudeField.setColumns(10);
+		}
+		{
+			JLabel lblLongitude = new JLabel("Longitude:");
+			GridBagConstraints gbc_lblLongitude = new GridBagConstraints();
+			gbc_lblLongitude.anchor = GridBagConstraints.EAST;
+			gbc_lblLongitude.insets = new Insets(0, 0, 5, 5);
+			gbc_lblLongitude.gridx = 2;
+			gbc_lblLongitude.gridy = 8;
+			patchProjectPanel.add(lblLongitude, gbc_lblLongitude);
+		}
+		{
+			longitudeField = new JTextField();
+			GridBagConstraints gbc_longitudeField = new GridBagConstraints();
+			gbc_longitudeField.fill = GridBagConstraints.HORIZONTAL;
+			gbc_longitudeField.insets = new Insets(0, 0, 5, 5);
+			gbc_longitudeField.gridx = 3;
+			gbc_longitudeField.gridy = 8;
+			patchProjectPanel.add(longitudeField, gbc_longitudeField);
+			longitudeField.setColumns(10);
+		}
+		
+				
+		
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
--- src/main/java/swing/PatchUser.java
+++ src/main/java/swing/PatchUser.java
@@ -2,15 +2,34 @@ package swing;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Image;
+import java.awt.Insets;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
 public class PatchUser extends JDialog {
 
-	private final JPanel contentPanel = new JPanel();
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private final JPanel patchUserPanel = new JPanel();
+	private JTextField nameField;
+	private JTextField passwordField;
+	private JTextField newPasswordField;
+	private JTextField validateNewPassField;
 
 	/**
 	 * Launch the application.
@@ -18,6 +37,8 @@ public class PatchUser extends JDialog {
 	public static void main(String[] args) {
 		try {
 			PatchUser dialog = new PatchUser();
+			//definimos o título da janel
+			dialog.setTitle("Patch User");
 			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 			dialog.setVisible(true);
 		} catch (Exception e) {
@@ -27,13 +48,160 @@ public class PatchUser extends JDialog {
 
 	/**
 	 * Create the dialog.
+	 * @throws IOException 
 	 */
-	public PatchUser() {
-		setBounds(100, 100, 450, 300);
+	public PatchUser() 
+	{
+		//Definição da Caixa de Diálogo
+		setBounds(100, 100, 636, 387);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
-		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
-		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		patchUserPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(patchUserPanel, BorderLayout.CENTER);
+		
+		// Definição do Painel interno
+		GridBagLayout gbl_patchUserPanel = new GridBagLayout();  //Layout Manager
+		gbl_patchUserPanel.columnWidths = new int[]{20, 34, 10, 100, 200, 0, 0, 0, 0};
+		gbl_patchUserPanel.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_patchUserPanel.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_patchUserPanel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		patchUserPanel.setLayout(gbl_patchUserPanel);
+		
+		
+		     //Inserir imagem : user -> Path e localização no Painel
+		{ 
+			JLabel userLabel = new JLabel("");
+			
+			Image user = new ImageIcon(this.getClass().getResource("/user.jpg")).getImage();
+			userLabel.setIcon(new ImageIcon(user));
+			GridBagConstraints gbc_lblUser = new GridBagConstraints();
+			gbc_lblUser.gridheight = 2;
+			gbc_lblUser.insets = new Insets(0, 0, 5, 5);
+			gbc_lblUser.anchor = GridBagConstraints.SOUTHEAST;
+			gbc_lblUser.gridx = 1;
+			gbc_lblUser.gridy = 3;
+			patchUserPanel.add(userLabel, gbc_lblUser);
+
+		}
+		
+		// Informação sobre o user que está a usar o programa
+		{
+			JLabel lblWellcome = new JLabel("Bem-vindo");
+			GridBagConstraints gbc_lblWellcome = new GridBagConstraints();
+			gbc_lblWellcome.anchor = GridBagConstraints.BELOW_BASELINE;
+			gbc_lblWellcome.insets = new Insets(0, 0, 5, 5);
+			gbc_lblWellcome.gridx = 6;
+			gbc_lblWellcome.gridy = 1;
+			patchUserPanel.add(lblWellcome, gbc_lblWellcome);
+		}
+		{
+			JLabel lblRegistryuser = new JLabel("RegistryUser");
+			GridBagConstraints gbc_lblRegistryuser = new GridBagConstraints();
+			gbc_lblRegistryuser.insets = new Insets(0, 0, 5, 0);
+			gbc_lblRegistryuser.gridx = 7;
+			gbc_lblRegistryuser.gridy = 1;
+			patchUserPanel.add(lblRegistryuser, gbc_lblRegistryuser);
+		}
+		
+		// Título do Comando : Post User
+		{
+//			JLabel lblPostUser = DefaultComponentFactory.getInstance().createTitle("Post User");
+//			lblPostUser.setFont(new Font("OCR A Extended", Font.BOLD | Font.ITALIC, 14));
+			GridBagConstraints gbc_lblPostUser = new GridBagConstraints();
+			gbc_lblPostUser.insets = new Insets(0, 0, 5, 5);
+			gbc_lblPostUser.gridx = 3;
+			gbc_lblPostUser.gridy = 0;
+//			postUserPanel.add(lblPostUser, gbc_lblPostUser);
+		}
+		
+		    // Labels e campos a ser preenchidos
+		{
+			JLabel lblName = new JLabel("Username:");
+			GridBagConstraints gbc_lblName = new GridBagConstraints();
+			gbc_lblName.anchor = GridBagConstraints.SOUTHEAST;
+			gbc_lblName.insets = new Insets(0, 0, 5, 5);
+			gbc_lblName.gridx = 3;
+			gbc_lblName.gridy = 3;
+			patchUserPanel.add(lblName, gbc_lblName);
+		}
+		{
+			nameField = new JTextField();
+			GridBagConstraints gbc_nameField = new GridBagConstraints();
+			gbc_nameField.fill = GridBagConstraints.HORIZONTAL;
+			gbc_nameField.insets = new Insets(0, 0, 5, 5);
+			gbc_nameField.anchor = GridBagConstraints.SOUTH;
+			gbc_nameField.gridx = 4;
+			gbc_nameField.gridy = 3;
+			patchUserPanel.add(nameField, gbc_nameField);
+			nameField.setColumns(10);
+		}
+		
+		{
+			JLabel lblOldPassword = new JLabel("Old Password:");
+			lblOldPassword.setToolTipText("Minimum 4 characters");
+			GridBagConstraints gbc_lblOldPassword = new GridBagConstraints();
+			gbc_lblOldPassword.anchor = GridBagConstraints.EAST;
+			gbc_lblOldPassword.insets = new Insets(0, 0, 5, 5);
+			gbc_lblOldPassword.gridx = 3;
+			gbc_lblOldPassword.gridy = 4;
+			patchUserPanel.add(lblOldPassword, gbc_lblOldPassword);
+			lblOldPassword.setToolTipText("Minimum 4 character.");
+
+		}
+		{
+			passwordField = new JTextField();
+			GridBagConstraints gbc_oldPasswordField = new GridBagConstraints();
+			gbc_oldPasswordField.fill = GridBagConstraints.HORIZONTAL;
+			gbc_oldPasswordField.insets = new Insets(0, 0, 5, 5);
+			gbc_oldPasswordField.gridx = 4;
+			gbc_oldPasswordField.gridy = 4;
+			patchUserPanel.add(passwordField, gbc_oldPasswordField);
+			passwordField.setColumns(10);
+			passwordField.setToolTipText("Minimum 4 character.");
+		}
+	
+		{
+			JLabel lblNewPassword = new JLabel("New Password:");
+			lblNewPassword.setToolTipText("Minimum 4 characters");
+			GridBagConstraints gbc_lblNewPassword = new GridBagConstraints();
+			gbc_lblNewPassword.anchor = GridBagConstraints.EAST;
+			gbc_lblNewPassword.insets = new Insets(0, 0, 5, 5);
+			gbc_lblNewPassword.gridx = 3;
+			gbc_lblNewPassword.gridy = 5;
+			patchUserPanel.add(lblNewPassword, gbc_lblNewPassword);
+		}
+		{
+			newPasswordField = new JTextField();
+			GridBagConstraints gbc_newPasswordField = new GridBagConstraints();
+			gbc_newPasswordField.fill = GridBagConstraints.HORIZONTAL;
+			gbc_newPasswordField.insets = new Insets(0, 0, 5, 5);
+			gbc_newPasswordField.gridx = 4;
+			gbc_newPasswordField.gridy = 5;
+			patchUserPanel.add(newPasswordField, gbc_newPasswordField);
+			newPasswordField.setColumns(10);
+		}
+		
+		{
+			JLabel lblValidateNewPass = new JLabel("Confirm New Password:");
+			lblValidateNewPass.setToolTipText("Minimum 4 characters");
+			GridBagConstraints gbc_lblValidateNewPass = new GridBagConstraints();
+			gbc_lblValidateNewPass.anchor = GridBagConstraints.EAST;
+			gbc_lblValidateNewPass.insets = new Insets(0, 0, 5, 5);
+			gbc_lblValidateNewPass.gridx = 3;
+			gbc_lblValidateNewPass.gridy = 6;
+			patchUserPanel.add(lblValidateNewPass, gbc_lblValidateNewPass);
+		}
+		
+		{
+			validateNewPassField = new JTextField();
+			GridBagConstraints gbc_validateNewPassField = new GridBagConstraints();
+			gbc_validateNewPassField.fill = GridBagConstraints.HORIZONTAL;
+			gbc_validateNewPassField.insets = new Insets(0, 0, 5, 5);
+			gbc_validateNewPassField.gridx = 4;
+			gbc_validateNewPassField.gridy = 6;
+			patchUserPanel.add(validateNewPassField, gbc_validateNewPassField);
+		}
+		
+		
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
