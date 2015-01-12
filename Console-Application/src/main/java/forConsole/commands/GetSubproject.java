afe32c4cd3bf7b33f3ab391255e1bdca444f0b8f
Lucas <lucas.andrade234@gmail.com>
2015-01-12 17:41:02 +0000

    tentativa de por os poms a funcionar

---
+++ Console-Application/src/main/java/forConsole/commands/GetSubproject.java
@@ -0,0 +1,136 @@
+package app.commands;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.Callable;
+
+import utils.Project;
+import app.commandParser.CommandParser;
+import app.elements.AppElement;
+import app.elements.Message;
+import app.repository.ProjectsRepository;
+
+/**
+ * Class whose instances are {@link Command}s that return the Sub{@link Project}
+ * s in a {@link Project}.
+ * Caller {@code String}: GET /project/{pid}/subproject
+ * 
+ * @author Filipa Gonçalves, Filipe Maia, Lucas Andrade.
+ * @since 08/12/2014
+ */
+public class GetSubproject extends BaseCommandResultsOutputMethod
+{
+
+	/**
+	 * The {@link ProjectsRepository} with the {@code Project}s. This
+	 * {@code ProjectRepository} is accessed to get the {@code Project} with the
+	 * wanted Sub{@code Project}s.
+	 */
+	private final ProjectsRepository repository;
+
+	/**
+	 * {@code String} with the {@code Project}ID argument's name. The
+	 * {@link CommandParser.Node.content} to be used (between "{" and "}", see
+	 * {@link CommandParser#Node#isPlaceHolderNode()}) in one of the
+	 * {@link CommandParser#Node}s of this {@code Command}'s Path.
+	 */
+	public static final String PID = "pid";
+
+	/**
+	 * An array of {@code String}s with the names of all mandatory arguments.
+	 */
+	public static final String[] DEMANDING_PARAMETERS = new String[] { PID };
+
+	/**
+	 * Class that implements the {@code GetSubproject} factory, according to the
+	 * {@link CommandFactory}.
+	 */
+	public static class Factory implements CommandFactory
+	{
+
+		/**
+		 * The {@link ProjectsRepository} with the {@code Project}s. This
+		 * {@code ProjectRepository} is accessed to get the {@code Project} with
+		 * the wanted Sub{@code Project}s.
+		 */
+		private final ProjectsRepository repository;
+
+		/**
+		 * The constructor for {@code Factory}.
+		 * 
+		 * @param repository
+		 *            The {@code ProjectRepository} with the {@code Project}.
+		 */
+		public Factory(ProjectsRepository repository)
+		{
+			this.repository = repository;
+		}
+
+		/**
+		 * @see CommandFactory#newInstance(Map)
+		 */
+		@Override
+		public Callable<Result> newInstance(Map<String, String> parameters)
+		{
+			return new GetSubproject(repository, parameters);
+		}
+	}
+
+	/**
+	 * The constructor for {@code GetSubproject}.
+	 * 
+	 * @param repository    The {@code ProjectRepository}.
+	 * @param parameters    The {@code Command} arguments.
+	 */
+	public GetSubproject(ProjectsRepository repository,
+			Map<String, String> parameters)
+	{
+		super(parameters);
+		this.repository = repository;
+	}
+
+	/**
+	 * @see app.commands.BaseCommand#getMandatoryParameters()
+	 */
+	@Override
+	protected String[] getMandatoryParameters()
+	{
+		return DEMANDING_PARAMETERS;
+	}
+
+	/**
+	 * Get's the Sub{@code Project}s from the {@code Project} with the argument
+	 * PID stored in {@link GetSubproject#parameters} (argument's name is
+	 * {@link GetSubproject#PID}), if the {@code Project} exists and has at
+	 * least one Sub{@code Project}).
+	 * 
+	 * @return An array of {@code DatabaseElement} with all the sub{@code Project}s
+	 */
+	@Override
+	protected AppElement[] internalCall() throws Exception
+	{
+		Project project = repository.getProjectById(getParameterAsLong(PID));
+		if(project == null)
+			return new AppElement[]{new Message("Project with ID: " + getParameterAsLong(PID) 
+					+ " was not found!")};
+			
+		int subprojectsNumber = project.getSubprojectsNumber();
+		if(subprojectsNumber == 0)
+			return new AppElement[]{new Message("Project with ID: " + getParameterAsLong(PID) 
+					+ " has no subprojects.")};
+		
+		Collection<Project> subprojects = project.getContainerProject();
+		AppElement[] subprojectAux = new AppElement[subprojectsNumber];
+		int i = 0;
+		
+		for (Project subproject : subprojects)
+			subprojectAux[i++] = subproject;
+
+		return subprojectAux;
+	}
+
+
+
+	
+
+}
---
+++ Console-Application/src/main/java/forConsole/commands/PatchUser.java
@@ -0,0 +1,164 @@
+package app.commands;
+
+import java.util.Map;
+import java.util.concurrent.Callable;
+
+import app.elements.AppElement;
+import app.elements.Message;
+import app.elements.User;
+import app.elements.UserInterface;
+import app.repository.UserRepository;
+
+/**
+ * Class whose instances are commands that modifies {@link User}s
+ * {@code password}. Caller {@code String}: PATCH /users/{username} {parameter
+ * list}
+ * 
+ * @author Filipa Gonçalves, Filipe Maia, Lucas Andrade.
+ * @since 05/01/2015
+ */
+public class PatchUser extends BaseCommandUserAuthentication
+{
+
+	/**
+	 * {@code String} with the to be added {@code User}'s Username argument.
+	 */
+	private String username;
+
+	/**
+	 * {@code String} with the to be added {@code User}'s Password argument.
+	 */
+	private String newPassword;
+
+	/**
+	 * {@code String} with the {@code User} Username argument's name.
+	 */
+	public static final String USERNAME = "username";
+
+	/**
+	 * {@code String} with the {@code User} Password argument's name.
+	 */
+	private static final String NEWPASSWORD = "newPassword";
+
+	/**
+	 * {@code String} with the {@code User} Password argument's name.
+	 */
+	private static final String OLDPASSWORD = "oldPassword";
+
+	/**
+	 * An array of {@code String}s with the names of all mandatory arguments.
+	 */
+	private static final String[] DEMANDING_PARAMETERS = { USERNAME,
+			OLDPASSWORD, NEWPASSWORD };
+
+	/**
+	 * The {@link UserRepository} with the {@code User}s. The created
+	 * {@code User}s are stored in this {@code UserRepository}. Also, the
+	 * {@code User}'s Username is checked to see if the {@code User} already
+	 * exists in the {@code UserRepository}.
+	 */
+	private final UserRepository repository;
+
+	/**
+	 * Class that implements the {@link PostUsers} factory, according to the
+	 * {@link CommandFactory}.
+	 */
+	public static class Factory implements CommandFactory
+	{
+
+		/**
+		 * The {@link UserRepository} with the {@code User}s. The created
+		 * {@code User}s are stored in this {@code UserRepository}. Also, the
+		 * {@code User}'s Username is checked to see if the {@code User} already
+		 * exists in the {@code UserRepository}.
+		 * 
+		 * @see BasePostCommand#repository
+		 */
+		private final UserRepository uRepository;
+
+		/**
+		 * The constructor for {@code Factory}.
+		 * 
+		 * @param uRepository
+		 *            The {@code UserRepository}.
+		 */
+		public Factory(UserRepository uRepository)
+		{
+			this.uRepository = uRepository;
+		}
+
+		/**
+		 * @see CommandFactory#newInstance(Map)
+		 */
+		@Override
+		public Callable<Result> newInstance(Map<String, String> parameters)
+		{
+			return new PatchUser(uRepository, parameters);
+
+		}
+	}
+
+	/**
+	 * The constructor for {@code PostUsers}.
+	 * 
+	 * @param repository
+	 *            The {@code UserRepository}.
+	 * @param parameters
+	 *            The {@code Command} arguments.
+	 */
+	public PatchUser(UserRepository repository, Map<String, String> parameters)
+	{
+		super(repository, parameters);
+		this.repository = repository;
+	}
+
+	/**
+	 * @return The modified {@code User}.
+	 * @see BaseCommandUserAuthentication#internalCall()
+	 */
+	@Override
+	protected AppElement[] internalCall() throws Exception
+	{
+
+		this.newPassword = getParameterAsString(NEWPASSWORD);
+		String oldPassword = parameters.get(OLDPASSWORD);
+		this.username = getParameterAsString(USERNAME);
+
+		if (!super.authenticateUser(this.username, oldPassword))
+			return new AppElement[] { new Message(
+					"Old password is not correct for user: " + username) };
+		;
+
+		UserInterface user = repository.getUserByUsername(username);
+		AppElement[] messageAux = new AppElement[1];
+
+		if (user == null)
+		{
+			Message message = new Message("User not found!");
+			messageAux[0] = message;
+			return messageAux;
+		}
+
+		if (user.setNewPassword(newPassword))
+		{
+			Message message = new Message("Password successfully changed");
+			messageAux[0] = message;
+		} else
+		{
+			Message message = new Message(
+					"New password must at least have 4 characters.");
+			messageAux[0] = message;
+		}
+		return messageAux;
+	}
+
+	/**
+	 * @see app.commands.BaseCommand#getMandatoryParameters()
+	 */
+	@Override
+	protected String[] getMandatoryParameters()
+	{
+		return DEMANDING_PARAMETERS;
+	}
+
+}
