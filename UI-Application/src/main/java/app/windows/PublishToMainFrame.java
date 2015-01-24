26387ea3dda6d4ec7a7aa2609fdde6f66acaf36f
Lucas <lucas.andrade234@gmail.com>
2015-01-24 16:59:49 +0000

    documentação dos publishers

--- UI-Application/src/main/java/app/windows/PublishToMainFrame.java
+++ UI-Application/src/main/java/app/windows/PublishToMainFrame.java
@@ -10,15 +10,39 @@ import javax.swing.JTree;
 import app.AppElement;
 import app.windows.mainFrameAL.mainFrame.MainFrame;
 
+/**
+ * Allows to publish information about {@code AppElement}s into the right panel
+ * of the {@code MainFrame}. This class presents that information as a {@code JTree}.
+ * 
+ * The conversion method used to parse the {@code AppElement}s into nodes in the
+ * {@code JTree} is general enough to work with all kinds of {@code AppElement}s.
+ * 
+ * @author Filipa Gonçalves, Filipe Maia, Lucas Andrade.
+ * @since 19/01/2015
+ *
+ */
 public class PublishToMainFrame extends ResultsPublisher{
 	
 	protected JSplitPane splitPane = MainFrame.getSplitPane();
 	protected JPanel mainPanel = new JPanel();
 	
+	/**
+	 * Constructs this {@code Publisher}, and sets the {@code mainPanel} to have a 
+	 * {@code BorderLayout}. This panel will be later set as the right panel of the
+	 * {@code MainFrame}.
+	 */
 	public PublishToMainFrame() {
 		mainPanel.setLayout(new BorderLayout(0, 0));
 	}
 	
+	/**
+	 * Constructs a {@code JTree} using the method {@code TreeBuilder#getTree}. This
+	 * {@code JTree} will be added to the {@code mainPanel} as its {@code CENTER} 
+	 * component. Then, the {@code mainPanel} is added to the {@code MainFrame}, and
+	 * the {@code LoadingDialog} is disposed of, if any was set visible.
+	 * 
+	 * @see ResultsPublisher#publish(AppElement[])
+	 */
 	@Override
 	public void publish(AppElement[] appElements) {
 		JTree tree = TreeBuilder.getTree(appElements);
--- UI-Application/src/main/java/app/windows/PublishWithLoadingDialog.java
+++ UI-Application/src/main/java/app/windows/PublishWithLoadingDialog.java
@@ -4,23 +4,49 @@ import java.util.List;
 
 import app.windows.mainFrameAL.mainFrame.LoadingDialog;
 
-public abstract class PublishWithLoadingDialog implements Publisher{
-	
+/**
+ * Super class of all {@code Publishers} that use a {@code LoadingDialog}, while
+ * lengthy work is occurring. The {@code LoadingDialog} is disposed when the
+ * method {@code publish} is called (be it in an {@code ErrorPublisher}, or in a
+ * {@code ResultsPublisher}).
+ * 
+ * @author Filipa Gonçalves, Filipe Maia, Lucas Andrade.
+ * @since 19/01/2015
+ *
+ */
+public abstract class PublishWithLoadingDialog implements Publisher {
+
+	/**
+	 * Flag that indicates whether a {@code LoadingDialog} has been created or not.
+	 */
 	private boolean loadingPublishedFlag;
+
 	private LoadingDialog loadingDialog;
-	
-	public PublishWithLoadingDialog(){
+
+	/**
+	 * Constructs the publisher, and sets the {@code loadingPublishedFlag} to
+	 * false.
+	 */
+	public PublishWithLoadingDialog() {
 		loadingPublishedFlag = false;
 	}
-	
-	public void publish(List<String> chunks){
+
+	/**
+	 * Constructs and sets visible a new {@code LoadingDialog}, where the text
+	 * message will be the first element of {@code chunks}.
+	 * 
+	 * @param chunks
+	 *            Contains the message to be presented in the
+	 *            {@code LoadingDialog}
+	 */
+	public void publish(List<String> chunks) {
 		loadingPublishedFlag = true;
 		loadingDialog = new LoadingDialog(chunks.get(0));
 		loadingDialog.setVisible(true);
 	}
-	
+
 	protected void disposeLoadingDialog() {
-		if(loadingPublishedFlag) {
+		if (loadingPublishedFlag) {
 			loadingDialog.dispose();
 		}
 	}
