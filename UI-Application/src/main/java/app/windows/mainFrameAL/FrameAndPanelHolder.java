5072318cb67da1e9dbde99f25eddd87aed5b323c
Lucas <lucas.andrade234@gmail.com>
2015-01-27 16:22:03 +0000

    FrameAndPanelHolder ligeiramente melhorado

--- UI-Application/src/main/java/app/windows/mainFrameAL/FrameAndPanelHolder.java
+++ UI-Application/src/main/java/app/windows/mainFrameAL/FrameAndPanelHolder.java
@@ -1,10 +1,7 @@
 package app.windows.mainFrameAL;
 
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
 
 import javax.swing.JPanel;
 
@@ -24,13 +21,28 @@ import app.windows.commandWindowsAL.commandWindows.PostSubprojectFrame;
 import app.windows.commandWindowsAL.commandWindows.PostUserFrame;
 import app.windows.commandWindowsAL.commandWindows.PostWorkerInProjectFrame;
 
+/**
+ * This class instantiates all {@code MainDialogFrame} and {@code MainGetPanel} that the {@code MainFrame}
+ * may need during its lifetime. This class also has the responsibility to hide and reset any 
+ * {@code MainDialogFrame} when another {@code MainDialogFrame} or {@code MainGetPanel} is requested by 
+ * any of the {@code MainFrame}'s {@code ActionListener}s. 
+ * 
+ * @author Filipa Gonçalves, Filipe Maia, Lucas Andrade.
+ * @since 27/01/2015
+ *
+ */
 public class FrameAndPanelHolder {
 	
 	private static final Map<String, MainDialogFrame> DIALOGS = new HashMap<String, MainDialogFrame>();
 	private static final Map<String, MainGetPanel> PANELS = new HashMap<String, MainGetPanel>();
-	private static Set<Entry<String, MainDialogFrame>> dialogsSet; 
-	private static Set<Entry<String, MainGetPanel>> panelsSet; 
 	
+	private static MainDialogFrame lastDialog;
+	private static MainGetPanel lastPanel;
+	
+	/**
+	 * Constructs a new {@code FrameAndPanelHolder}, instantiates all the {@code MainDialogFrame}s 
+	 * and {@code MainGetPanel}s, and puts them in the respective {@code Map}s.
+	 */
 	public FrameAndPanelHolder() {
 		
 		DIALOGS.put("PostWorkerInProject", new PostWorkerInProjectFrame());
@@ -42,44 +54,68 @@ public class FrameAndPanelHolder {
 		DIALOGS.put("PostProject", new PostProjectFrame());
 		DIALOGS.put("PostSubproject", new PostSubprojectFrame());
 		DIALOGS.put("PostUser", new PostUserFrame());
-		dialogsSet = DIALOGS.entrySet();
 		
 		PANELS.put("GetWorkersInProject", new GetWorkersInProjectPanel());
 		PANELS.put("GetUser", new GetUserPanel());
 		PANELS.put("GetSubprojects", new GetSubprojectsPanel());
 		PANELS.put("GetProjects", new GetProjectsPanel());
 		
-		panelsSet = PANELS.entrySet();
+		lastDialog = new PatchUserFrame(); //to avoid using ifs or null pointer exceptions
+		lastPanel = new GetUserPanel();
 	}
 	
+	/**
+	 * Sets the {@code MainDialogFrame} visible. They are searched for by name:
+	 * 
+	 * "PostWorkerInProject" corresponds to a {@code PostWorkerInProjectFrame}
+	 * "DeleteProject" corresponds to a {@code DeleteProjectFrame}
+	 * "PatchConsultant" corresponds to a {@code PatchConsultantFrame}
+	 * "PatchProject" corresponds to a {@code PatchProjectFrame}
+	 * "PatchUser" corresponds to a {@code PatchUserFrame}
+	 * "PostConsultant" corresponds to a {@code PostConsultantFrame}
+	 * "PostProject" corresponds to a {@code PostProjectFrame}
+	 * "PostSubproject" corresponds to a {@code PostSubprojectFrame}
+	 * "PostUser" corresponds to a {@code PostUserFrame}
+	 * 
+	 * @param dialogName 
+	 * 				The name of the dialog to be set visible.
+	 */
 	public static void setDialogVisible(String dialogName) {
-		setAllNotVisible();
+		setNotVisibleAndReset();
 		MainDialogFrame dialog = DIALOGS.get(dialogName);
 		dialog.setFrameUser();
 		dialog.pack();
 		dialog.setVisible(true);
+		lastDialog = dialog;
 	}
 
+	/**
+	 * Gets a {@code MainGetPanel}. They are searched for by name:
+	 * 
+	 * "GetWorkersInProject" corresponds to a {@code GetWorkersInProjectPanel}
+	 * "GetUser" corresponds to a {@code GetUserPanel}
+	 * "GetSubprojects" corresponds to a {@code GetSubprojectsPanel}
+	 * "GetProjects" corresponds to a {@code GetProjectsPanel}
+	 * 
+	 * @param panelName
+	 * 			The name of the panel.
+	 * @return The requested {@code JPanel}
+	 */
 	public static JPanel getPanel(String panelName) {
-		setAllNotVisible();
+		setNotVisibleAndReset();
 		MainGetPanel panel = PANELS.get(panelName);
 		panel.setFrameUser();
+		lastPanel = panel;
 		return panel;
 	}
-	
-	private static void setAllNotVisible() {
-		Iterator<Entry<String, MainDialogFrame>> dialogsIterator = dialogsSet.iterator();
-		while(dialogsIterator.hasNext() ){
-			MainDialogFrame frame = dialogsIterator.next().getValue();
-			if(frame.isVisible()) {
-				frame.setVisible(false);
-				frame.resetAllFields();
-			}
-		}
-		
-		Iterator<Entry<String, MainGetPanel>> panelsIterator = panelsSet.iterator();
-		while(panelsIterator.hasNext()) {
-			panelsIterator.next().getValue().resetAllFields();
-		}
+
+	/**
+	 * Sets the last used {@code MainDialogFrame} and {@code MainGetPanel}, to
+	 * not visible, and resets all their text fields. 
+	 */
+	private static void setNotVisibleAndReset() {
+		lastDialog.setVisible(false);
+		lastDialog.resetAllFields();
+		lastPanel.resetAllFields();
 	}
 }
