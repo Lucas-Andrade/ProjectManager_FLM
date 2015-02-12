36aabedba33acfa954ae5880029fe3a314a4702f
filipe-maia <filipemaia@msn.com>
2015-02-12 18:00:42 +0000

    servlet melhoramentos

--- Applications/DistributedApplication/Server/src/main/java/server/ProjectManagerServlet.java
+++ Applications/DistributedApplication/Server/src/main/java/server/ProjectManagerServlet.java
@@ -27,108 +27,69 @@ import app.domainCommands.exceptions.NoSuchUsernameException;
 
 @SuppressWarnings("serial")
 public class ProjectManagerServlet extends HttpServlet {
-	
 
-	
-		/**
-		 * 
-		 * @param req
-		 * @param resp
-		 * @throws IOException
-		 * @throws ServletException
-		 */
-    @Override
-	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+	/**
+	 * 
+	 * @param req
+	 * @param resp
+	 * @throws IOException
+	 * @throws ServletException
+	 */
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException, ServletException {
 		String method = req.getMethod();
-		if(method.equals("PATCH")) {
+		if (method.equals("PATCH")) {
 			doPatch(req, resp);
 			return;
-		}else{
+		} else {
 			super.service(req, resp);
 		}
-    }
-    
-    /**
-     * Turns the array of {@code AppElement}s into a string in the JSON format.
-     * @param result
-     * @return a string with the information contained in the array of {@code AppElement}s
-     */
-    private static String toString(AppElement[] result) {
-    	return toJSONObject(result).toString();
-    }
-    
-    /**
-     * Turns the array of {@code AppElement}s into a {@code JSONObject}.
-     * @param result
-     * @return a a {@code JSONObject} with the information contained in the array 
-     * 		of {@code AppElement}s
-     */
-    private static JSONObject toJSONObject(AppElement[] result) {
-    	JSONArray array;
-    	if(result.length == 1) {
-    		return result[0].getJson();
-    	} else {
-    		array = new JSONArray();
-    		for(int i = 0; i < result.length; i++){
-    			array.put(result[i].getJson());
-    		}
-     	}
-    	return array.toJSONObject(array);
-    }
-	 
-    /**
-     * 
-     * @param req
-     * @param resp
-     * @throws IOException
-     */
+	}
+
+	/**
+	 * Turns the array of {@code AppElement}s into a string in the JSON format.
+	 * 
+	 * @param result
+	 * @return a string with the information contained in the array of
+	 *         {@code AppElement}s
+	 */
+	private static String toString(AppElement[] result) {
+		return toJSONObject(result).toString();
+	}
+
+	/**
+	 * Turns the array of {@code AppElement}s into a {@code JSONObject}.
+	 * 
+	 * @param result
+	 * @return a a {@code JSONObject} with the information contained in the
+	 *         array of {@code AppElement}s
+	 */
+	private static JSONObject toJSONObject(AppElement[] result) {
+		JSONArray array;
+		if (result.length == 1) {
+			return result[0].getJson();
+		} else {
+			array = new JSONArray();
+			for (int i = 0; i < result.length; i++) {
+				array.put(result[i].getJson());
+			}
+		}
+		return array.toJSONObject(array);
+	}
+
+	/**
+	 * 
+	 * @param req
+	 * @param resp
+	 * @throws IOException
+	 */
 	@Override
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
+		doGetAndDelete(req, resp);
+	}
 
-		System.out.println(); //For test purposes.
-		System.out.println("New Connection Received:"); //For test purposes.
-
-		String parameters = req.getQueryString();
-		String path = getCommandStringFromRequest(req);
-		String method = getCommandMethodFromRequest(req);
-		
-		System.out.println(method + " - " + path + " - " + parameters); //For test purposes.
-
-		CommandParser cp = null;
-		String input = "";
-		try {
-			cp = getCommandParser();
-			cp.getCommand("GET", "/authenticate", parameters).call();
-			Result pr = cp.getCommand(method, path, parameters).call();
-			if (req.getHeader("Accept").contains("text/html"))
-			{
-				new ToTextHtml().parse(toJSONObject(pr.getResults()));
-			}
-			else
-			{
-				input = toString(pr.getResults());
-			}
-			resp.setStatus(200);
-		} catch (Exception e) {
-			getErrorMessage(resp, e);
-		}
-
-		try //For test purposes.
-		{ //For test purposes.
-			int numBytes = Integer.parseInt(req.getHeader("Content-length")); //For test purposes.
-			byte[] bytes = new byte [numBytes]; //For test purposes.
-			req.getInputStream().read(bytes); //For test purposes.
-			System.out.println("IO contents: " + new String(bytes)); //For test purposes.
-		} //For test purposes.
-		catch (NumberFormatException e){} //For test purposes.
-		System.out.println(input); //For test purposes.
-
-		OutputStream out = resp.getOutputStream();
-		out.write(input.getBytes());
-    }
-
-	
 	/**
 	 * 
 	 * @param req
@@ -138,166 +99,122 @@ public class ProjectManagerServlet extends HttpServlet {
 	@Override
 	public void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
+		doPostAndPatch(req, resp);
+	}
 
-		System.out.println(); //For test purposes.
-		System.out.println("New Connection Received:"); //For test purposes.
-
+	private String getInputStreamContents(HttpServletRequest req) throws IOException{
 		InputStream inputStream = req.getInputStream();
-		
 		int numBytes = Integer.parseInt(req.getHeader("Content-length"));
-		byte[] bytes = new byte [numBytes];
+		byte[] bytes = new byte[numBytes];
 		inputStream.read(bytes);
-		
-		String parameters = new String(bytes) + "&accept="+ req.getHeader("Accept");
-		resp.setContentType("application/json");
-		
-		
-		String path = getCommandStringFromRequest(req);
-		String method = getCommandMethodFromRequest(req);
-		
-		System.out.println(method + " - " + path + " - " + parameters); //For test purposes.
-
-		CommandParser cp = null;
-		String input = "";
+		return new String(bytes);
+	}
+	
+	private void callAuthenticateCommand(String parameters, CommandParser cp) throws Exception{
+		cp.getCommand("GET", "/authenticate", parameters).call();
+	}
+	
+	private String callRequestedCommand(String method, String path, String parameters, HttpServletRequest req, HttpServletResponse resp) throws IOException{
 		try {
-			cp = getCommandParser();
+			CommandParser cp = getCommandParser();
+			if(method.equals("GET"))
+				callAuthenticateCommand(parameters, cp);
 			Result pr = cp.getCommand(method, path, parameters).call();
-			if (req.getHeader("Accept").contains("text/html"))
-			{
-				for (AppElement elem : pr.getResults())
-				{
-					input += new ToTextHtml().parse(elem.getJson());
-				}
+			if (req.getHeader("Accept").contains("text/html")) {
+				return new ToTextHtml().parse(toJSONObject(pr.getResults()));
+			} else {
+				return toString(pr.getResults());
 			}
-			else
-			{
-				input = toString(pr.getResults());
-			}
-			resp.setStatus(200);
 		} catch (Exception e) {
 			getErrorMessage(resp, e);
 		}
+		return null;
+	}
+	
+	private void doPostAndPatch(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		System.out.println(); // For test purposes.
+		System.out.println("New Connection Received:"); // For test purposes.
 
-		System.out.println(method + " - " + path + " - IO constents: " + parameters); //For test purposes.
-		System.out.println(input); //For test purposes.
+		String method = getCommandMethodFromRequest(req);
+		String path = getCommandStringFromRequest(req);
+		
+		String parameters = getInputStreamContents(req) + "&accept="
+				+ req.getHeader("Accept");
+		resp.setContentType("application/json");
+
+		System.out.println(method + " - " + path + " - " + parameters); // For
+																		// test
+																		// purposes.
+
+		String input = callRequestedCommand(method, path, parameters, req, resp);
+		resp.setStatus(200);
+
+		System.out.println(method + " - " + path + " - IO constents: "
+				+ parameters); // For test purposes.
+		System.out.println(input); // For test purposes.
 
 		OutputStream out = resp.getOutputStream();
 		out.write(input.getBytes());
 	}
 
+	private void doGetAndDelete(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		System.out.println(); // For test purposes.
+		System.out.println("New Connection Received:"); // For test purposes.
 
-/**
- * 
- * @param req
- * @param resp
- * @throws IOException
- */
+		String method = getCommandMethodFromRequest(req);
+		String path = getCommandStringFromRequest(req);
+
+		String parameters = req.getQueryString();
+		
+		System.out.println(method + " - " + path + " - " + parameters); // For
+																		// test
+																		// purposes.
+
+		String input = callRequestedCommand(method, path, parameters, req, resp);
+		resp.setStatus(200);
+
+		try // For test purposes.
+		{ // For test purposes.
+			int numBytes = Integer.parseInt(req.getHeader("Content-length")); // For
+																				// test
+																				// purposes.
+			byte[] bytes = new byte[numBytes]; // For test purposes.
+			req.getInputStream().read(bytes); // For test purposes.
+			System.out.println("IO contents: " + new String(bytes)); // For test
+																		// purposes.
+		} // For test purposes.
+		catch (NumberFormatException e) { // For test purposes.
+		} // For test purposes.
+		System.out.println(input); // For test purposes.
+
+		OutputStream out = resp.getOutputStream();
+		out.write(input.getBytes());
+	}
+	
+	/**
+	 * 
+	 * @param req
+	 * @param resp
+	 * @throws IOException
+	 */
 	@Override
 	public void doDelete(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-
-		System.out.println(); //For test purposes.
-		System.out.println("New Connection Received:"); //For test purposes.
-
-		String parameters = req.getQueryString();
-		String path = getCommandStringFromRequest(req);
-		String method = getCommandMethodFromRequest(req);
-		
-		System.out.println(method + " - " + path + " - " + parameters); //For test purposes.
-
-		CommandParser cp = null;
-		String input = "";
-		try {
-			cp = getCommandParser();
-			Result pr = cp.getCommand(method, path, parameters).call();
-			if (req.getHeader("Accept").contains("text/html"))
-			{
-				for (AppElement elem : pr.getResults())
-				{
-					input += new ToTextHtml().parse(elem.getJson());
-				}
-			}
-			else
-			{
-				input = toString(pr.getResults());
-			}
-			resp.setStatus(200);
-		} catch (Exception e) {
-			getErrorMessage(resp, e);
-		}
-
-		System.out.println(method + " " + path + " " + parameters); //For test purposes.
-		try //For test purposes.
-		{ //For test purposes.
-			int numBytes = Integer.parseInt(req.getHeader("Content-length")); //For test purposes.
-			byte[] bytes = new byte [numBytes]; //For test purposes.
-			req.getInputStream().read(bytes); //For test purposes.
-			System.out.println("IO contents: " + new String(bytes)); //For test purposes.
-		} //For test purposes.
-		catch (NumberFormatException e){} //For test purposes.
-		System.out.println(input); //For test purposes.
-
-		OutputStream out = resp.getOutputStream();
-		out.write(input.getBytes());
+		doGetAndDelete(req, resp);
 	}
 
-	
-
-/**
- * 
- * @param req
- * @param resp
- * @throws IOException
- */
+	/**
+	 * 
+	 * @param req
+	 * @param resp
+	 * @throws IOException
+	 */
 	public void doPatch(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-
-		System.out.println(); //For test purposes.
-		System.out.println("New Connection Received:"); //For test purposes.
-
-		InputStream inputStream = req.getInputStream();
-		
-		int numBytes = Integer.parseInt(req.getHeader("Content-length"));
-		byte[] bytes = new byte [numBytes];
-		inputStream.read(bytes);
-		
-		String parameters = new String(bytes) + "&accept="+ req.getHeader("Accept");
-		resp.setContentType("application/json");
-		
-		
-		String path = getCommandStringFromRequest(req);
-		String method = getCommandMethodFromRequest(req);
-		
-		System.out.println(method + " - " + path + " - " + parameters); //For test purposes.
-
-		CommandParser cp = null;
-		String input = "";
-		try {
-			cp = getCommandParser();
-			Result pr = cp.getCommand(method, path, parameters).call();
-			if (req.getHeader("Accept").contains("text/html"))
-			{
-				for (AppElement elem : pr.getResults())
-				{
-					input += new ToTextHtml().parse(elem.getJson());
-				}
-			}
-			else
-			{
-				input = toString(pr.getResults());
-			}
-			resp.setStatus(200);
-		} catch (Exception e) {
-			getErrorMessage(resp, e);
-		}
-
-		System.out.println(method + " - " + path + " - IO constents: " + parameters); //For test purposes.
-		System.out.println(input); //For test purposes.
-
-		OutputStream out = resp.getOutputStream();
-		out.write(input.getBytes());
+		doPostAndPatch(req, resp);
 	}
-
 
 	/**
 	 * Gets the singleton {@link CommandParser}.
@@ -331,15 +248,19 @@ public class ProjectManagerServlet extends HttpServlet {
 		return req.getMethod();
 	}
 
-	
 	/**
-	 * Method responsable for verify which Status Error Code have the response from the server and send it's message to client.
-	 * @param resp provide HTTP Status Code and Error message.
-	 * @param e Exception
+	 * Method responsable for verify which Status Error Code have the response
+	 * from the server and send it's message to client.
+	 * 
+	 * @param resp
+	 *            provide HTTP Status Code and Error message.
+	 * @param e
+	 *            Exception
 	 * @return Error Message
-	 * @throws IOException 
+	 * @throws IOException
 	 */
-	private void getErrorMessage(HttpServletResponse resp, Exception e) throws IOException{
+	private void getErrorMessage(HttpServletResponse resp, Exception e)
+			throws IOException {
 		String message;
 		if (e.getMessage() == null)
 			message = "";
@@ -347,25 +268,29 @@ public class ProjectManagerServlet extends HttpServlet {
 			message = e.getMessage();
 		if (e.getClass().equals(UnknownCommandException.class)) {
 			resp.sendError(404, message);
-			System.out.println("Error " + 404 + " - " + message); //For test purposes.
-		} else if (e.getClass().equals(
-				InvalidCommandArgumentsException.class)
+			System.out.println("Error " + 404 + " - " + message); // For test
+																	// purposes.
+		} else if (e.getClass().equals(InvalidCommandArgumentsException.class)
 				|| e.getClass().equals(DuplicateArgumentsException.class)) {
 			resp.sendError(400, message);
-			System.out.println("Error " + 400 + " - " + message); //For test purposes.
+			System.out.println("Error " + 400 + " - " + message); // For test
+																	// purposes.
 		} else if (e.getClass().equals(InvalidUserException.class)
 				|| e.getClass().equals(NoSuchUsernameException.class)
-						|| e.getClass().equals(IncorrectPasswordException.class)) {
+				|| e.getClass().equals(IncorrectPasswordException.class)) {
 			resp.sendError(401, message);
-			System.out.println("Error " + 401 + " - " + message); //For test purposes.
+			System.out.println("Error " + 401 + " - " + message); // For test
+																	// purposes.
 		} else if (e.getClass().equals(
 				MandatoryParameterNotPresentException.class)) {
 			resp.sendError(400, message);
-			System.out.println("Error " + 400 + " - " + message); //For test purposes.
+			System.out.println("Error " + 400 + " - " + message); // For test
+																	// purposes.
 		} else {
 			resp.sendError(500, message);
-			System.out.println("Error " + 500 + " - " + message); //For test purposes.
+			System.out.println("Error " + 500 + " - " + message); // For test
+																	// purposes.
 		}
 	}
-	
+
 }
